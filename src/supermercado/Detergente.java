2c90440aa4da179f069477e4c3ae0232b4ee236f
wzseba <wzseba@gmail.com>
2024-09-27 16:40:53 -0300

    add: logica de interface

--- src/supermercado/Detergente.java
+++ src/supermercado/Detergente.java
@@ -1,5 +1,35 @@
 package supermercado;
 
-public class Detergente extends Producto {
+import interfaz.productos.ConDescuento;
+
+public class Detergente extends Producto implements ConDescuento {
+
+	private double descuento;
+
+	public Detergente(String marca, double precio) {
+		super(marca, precio);
+		this.descuento = 0;
+	}
+
+	@Override
+	public void setDescuento(double des) {
+		this.descuento = des;
+
+	}
+
+	@Override
+	public double getDescuento() {
+		return (1 - (this.descuento / 100));
+	}
+
+	@Override
+	public double getPrecioDescuento() {
+		return super.getPrecio() * this.getDescuento();
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " Precio con descuento: " + this.getPrecioDescuento();
+	}
 
 }
