8209321aabc5d6761b9289853d77604fd308db17
wzseba <wzseba@gmail.com>
2024-09-26 21:33:01 -0300

    add: estructura basica de la clase

--- src/supermercado/Producto.java
+++ src/supermercado/Producto.java
@@ -2,4 +2,28 @@ package supermercado;
 
 public class Producto {
 
+	private String marca;
+	private double precio;
+
+	public Producto(String marca, double precio) {
+		this.marca = marca;
+		this.precio = precio;
+	}
+
+	public String getMarca() {
+		return this.marca;
+	}
+
+	public double getPrecio() {
+		return this.precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	@Override
+	public String toString() {
+		return "Marca: " + this.marca + " -- " + "Precio: " + this.precio;
+	}
 }
